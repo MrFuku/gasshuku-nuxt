e0237ca46716b934810ca9ee7faf8039a376ecfb
MrFuku <mmafighter_shun@icloud.com>
2020-03-07 16:33:20 +0900

    コメント一覧表示画面実装

---
+++ pages/comments/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container">
+    <h1>Tweet App</h1>
+    <p><input
+        type="text"
+        name="content"
+      /></p>
+    <div>
+      <button>save</button>
+    </div>
+    <ul>
+      <li
+        v-for="comment in comments"
+        :key="comment.id"
+      >
+        <p>
+          <span>{{ comment.Content }} user by: {{ comment.UserId }}</span>
+        </p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comments: []
+    };
+  },
+  methods: {
+    async getComments() {
+      const url = "/comment";
+      const response = await this.$axios.$get(url);
+      this.comments = response;
+    }
+  },
+  mounted: function() {
+    this.getComments();
+  }
+};
+</script>
