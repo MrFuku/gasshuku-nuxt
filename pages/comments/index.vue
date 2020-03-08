c41dbb81047330455e93394f9d8a9e886f284460
MrFuku <mmafighter_shun@icloud.com>
2020-03-08 19:05:34 +0900

    コメント投稿機能実装
    ds

--- pages/comments/index.vue
+++ pages/comments/index.vue
@@ -1,38 +1,59 @@
 <template>
-  <section class="container">
-    <h1>Tweet App</h1>
-    <p><input
-        type="text"
-        name="content"
-      /></p>
-    <div>
-      <button>save</button>
-    </div>
-    <ul>
-      <li
-        v-for="comment in comments"
-        :key="comment.id"
-      >
-        <p>
-          <span>{{ comment.Content }} user by: {{ comment.UserId }}</span>
-        </p>
-      </li>
-    </ul>
-  </section>
+  <v-app>
+    <section class="container">
+      <h1>Tweet App</h1>
+      <v-col cols="6">
+        <v-text-field
+          v-model="editContent"
+          append-outer-icon="mdi-send"
+          outlined
+          label="tweetを投稿"
+          type="text"
+          @click:append-outer="sendComment"
+        ></v-text-field>
+      </v-col>
+      <ul>
+        <li
+          v-for="comment in comments"
+          :key="comment.id"
+        >
+          <p>
+            <span>{{ comment.Content }} user by: {{ comment.UserId }}</span>
+          </p>
+        </li>
+      </ul>
+    </section>
+  </v-app>
 </template>
 
 <script>
+import Comment from "~/components/Comment";
+
 export default {
+  components: {
+    Comment
+  },
   data() {
     return {
-      comments: []
+      comments: [],
+      editContent: ""
     };
   },
   methods: {
     async getComments() {
       const url = "/comment";
       const response = await this.$axios.$get(url);
-      this.comments = response;
+      this.comments = response.reverse();
+    },
+    async postComment(content) {
+      const url = "/comment";
+      const comment = await this.$axios.$post(url, `content=${content}`);
+      this.comments.unshift(comment);
+    },
+    sendComment() {
+      if (this.editContent === "") return;
+      this.postComment(this.editContent);
+      this.editContent = "";
     }
   },
   mounted: function() {
